6821d1cf74ecb2e288cc814a7973185d52569234
Muhammad Hassan <hassantariq.8631@gmail.com>
2015-10-26 17:41:55 -0500

    First Commit

---
+++ ngNinja.Web/Scripts/app/modules/candidate/js/onBoardingApp.candidate.controller.js
@@ -0,0 +1,56 @@
+﻿'use strict';
+
+(function () {
+    var app = angular.module('onBoardingApp.candidate.controllers', []);
+
+    app.controller('CandidateController', ['candidateService', function (candidateService) {
+        
+        var vm = this;
+
+        vm.findCandidate = function (id) {
+            for (var i in vm.candidates) {
+                if (vm.candidates[i].candidate_id == id)
+                    return i;
+            }
+        };
+
+        vm.tempValueForUnitTest = "testing123";
+
+        // Invoke your service layer for this module
+        vm.candidates = candidateService.getAllCandidates()
+            .then(function(data) {
+                vm.candidates = data;
+            });
+            //.catch(function (data, status) {
+            //    console.error('error', response.status, response.data);
+            //})
+            //.finally(function () {
+            //    console.log("finally");
+            //});
+
+        vm.currentEdit = {};
+
+        vm.add = function () {
+            vm.candidates.push(vm.newCandidate);
+            vm.newCandidate = null;
+        };
+
+        vm.edit = function (candidate) {
+            vm.currentEdit[candidate.candidate_id] = true;
+            vm.itemToEdit = angular.copy(candidate);
+        };
+
+        vm.cancelEdit = function (candidateId) {
+            vm.currentEdit[candidateId] = false;
+        };
+
+        vm.save = function (candidate) {
+            var c = vm.findCandidate(candidate.candidate_id);
+            vm.candidates[c] = vm.itemToEdit;
+            vm.currentEdit[candidate.candidate_id] = false;
+        };
+
+        vm.testFunctionForUnitTesting = function() { return "test"; }
+
+    }]);
+})();
